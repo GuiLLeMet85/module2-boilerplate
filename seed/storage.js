c6a78dd0839e5f57cc6e37db07a7bc050e751602
Dani <nneodani@gmail.com>
2022-07-01 18:55:18 +0200

    Process estorage Crud

--- seed/storage.js
+++ seed/storage.js
@@ -1,19 +1,19 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const Storage = require('../models/Storage');
 
 const storage = [
-    { boxname: "Box A", brickCategoryId: "", picture: "https://m.media-amazon.com/images/I/51m5htTsG9L._AC_SY450_.jpg"},
-    { boxname: "Box B", brickCategoryId: "", picture: "https://m.media-amazon.com/images/I/51qM3FzC6FL._AC_SY450_.jpg"},
-    { boxname: "Box C", brickCategoryId: "", picture: "https://m.media-amazon.com/images/I/81+leGYdbgL._AC_SX425_.jpg"},
-    { boxname: "Box D", brickCategoryId: "", picture: "https://m.media-amazon.com/images/I/51Fjuh1ldNL._AC_SX425_.jpg"}
+    { boxname: "Box A", brickCategoryId: [], picture: "https://m.media-amazon.com/images/I/51m5htTsG9L._AC_SY450_.jpg"},
+    { boxname: "Box B", brickCategoryId: [], picture: "https://m.media-amazon.com/images/I/51qM3FzC6FL._AC_SY450_.jpg"},
+    { boxname: "Box C", brickCategoryId: [], picture: "https://m.media-amazon.com/images/I/81+leGYdbgL._AC_SX425_.jpg"},
+    { boxname: "Box D", brickCategoryId: [], picture: "https://m.media-amazon.com/images/I/51Fjuh1ldNL._AC_SX425_.jpg"}
 ];
 
 //     { boxname: " ", brickId: "", quantity: , picture: " ", color: "", status: "", storageId: ""},
 
-const MONGO_URI = process.env.MONGODB_URI 
-
+const MONGO_URL = process.env.MONGO_URL
   mongoose
-    .connect(MONGO_URI)
+    .connect(MONGO_URL)
     .then(x => {
       console.log(
         `Connected to Mongo! Database name: "${x.connections[0].name}"`
