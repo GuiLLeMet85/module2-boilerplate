9e137e32cb6547aa9589a4d3089e2b579bb1cd32
Guillem B <guillembaracco@gmail.com>
2022-07-14 04:17:19 +0200

    menu style

--- routes/guestUser 2.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/pageGuest" ,  (req, res, next)=>{
-    res.render("guest/pageGuest")    
-
-})
-
-module.exports = router;
